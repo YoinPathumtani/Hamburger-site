410d257d08d98935077fe93b1762608be9f79fa7
yohei Shimaya <sankei.thailand@gmail.com>
2021-07-15 07:28:14 +0700

    メニュー開放を設定できました

--- js/style.js
+++ js/style.js
@@ -1,8 +1,8 @@
 $(function() {
   $('#is-open').click(function() {
-    $('#overlay').fadeIn(400, function () {
-      
-    });
+    $('#overlay').fadeIn(400);
+    $('#g-nav').css('right', 0);
   });
+  
 
 });
