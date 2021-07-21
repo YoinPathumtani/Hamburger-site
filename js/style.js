0ffd9cc5ba860496d00a5406b9b5a9586df79474
Yohei Shimaya <kawathailand@gmail.com>
2021-07-22 05:22:52 +0700

    フォントを修正しました

--- js/style.js
+++ js/style.js
@@ -2,11 +2,12 @@ $(function() {
   $('#is-open').click(function() {
     $('#overlay').fadeIn();
     $('#g-nav').css('right', 0);
+    $('body').css('position', 'fixed');
   });
   $('#is-close').on('click', function() {
     $('#overlay').fadeOut();
     $('#g-nav').css('right', '-100vw');
-
+    $('body').css('position', 'static');
   })
 
 });
