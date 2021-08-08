cefebd511c5331aac4e22bf9c677803b08557c66
yohei <kawathailand@gmail.com>
2021-08-08 17:06:12 +0700

    検索の位置を修正

--- search.php
+++ search.php
@@ -13,33 +13,33 @@
     <p class="c-text-card-p">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
   </section>
   <div class="l-section-search-result c-text-search-result">
-  <?php
-  if (isset($_GET['s']) && empty($_GET['s'])) { //検索ワードが未入力の場合は、
-    echo '検索キーワード未入力'; // '検索キーワード未入力'と出力してください。
-  } else { //そうでない場合は
-    echo '“' . $_GET['s'] . '”の検索結果：' . $wp_query->found_posts . '件'; // '検索ワードの検索結果'と件数を出力してください。
-  }
-  ?>
+    <?php
+    if (isset($_GET['s']) && empty($_GET['s'])) { //検索ワードが未入力の場合は、
+      echo '検索キーワード未入力'; // '検索キーワード未入力'と出力してください。
+    } else { //そうでない場合は
+      echo '“' . $_GET['s'] . '”の検索結果：' . $wp_query->found_posts . '件'; // '検索ワードの検索結果'と件数を出力してください。
+    }
+    ?>
+    <?php
+    if (have_posts()) :
+      while (have_posts()) :
+        the_post(); ?>
+        <section id="post-<?php the_ID(); ?>" class="p-card p-card-menu" <?php post_class(); ?>>
+          <?php the_post_thumbnail(); ?>
+          <div class="p-card-menuText c-text-card-menu">
+            <h2 class="c-text-card-menuName"><?php the_title(); ?></h2>
+            <?php the_excerpt(); ?>
+            <a href="<?php the_permalink(); ?>" class="c-button-detail">詳しく見る</a>
+          </div>
+        </section>
+      <?php endwhile;
+    else :
+      ?><p>表示する記事がありません</p>
+    <?php endif; ?>
+    <?php
+    if (function_exists('pagenation')) { // 関数が定義されていたらtrueになる
+      pagenation();
+    } ?>
   </div>
-  <?php
-  if (have_posts()) :
-    while (have_posts()) :
-      the_post(); ?>
-      <section id="post-<?php the_ID(); ?>" class="p-card p-card-menu" <?php post_class(); ?>>
-        <?php the_post_thumbnail(); ?>
-        <div class="p-card-menuText c-text-card-menu">
-          <h2 class="c-text-card-menuName"><?php the_title(); ?></h2>
-          <?php the_excerpt(); ?>
-          <a href="<?php the_permalink(); ?>" class="c-button-detail">詳しく見る</a>
-        </div>
-      </section>
-    <?php endwhile;
-  else :
-    ?><p>表示する記事がありません</p>
-  <?php endif; ?>
-  <?php
-  if (function_exists('pagenation')) { // 関数が定義されていたらtrueになる
-    pagenation();
-  } ?>
 </main><!-- l-main -->
 <?php get_footer(); ?>
