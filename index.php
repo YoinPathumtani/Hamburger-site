365ccc6bfa4ce037e1d7d930f7a61bcafb3d6fa4
yohei <kawathailand@gmail.com>
2021-08-08 18:04:09 +0700

    Take Out Eat Inリンクを修正

--- index.php
+++ index.php
@@ -1,44 +1,44 @@
 <?php get_header(); ?>
 <?php get_sidebar(); ?>
-  <main class="l-main l-main-fr p-main-fr">
-    <section class="l-section-mv p-mainVisual-fr p-mainVisual">
-      <div>
-        <h2 class="c-text-mv c-text-mvTitle">ダミーサイト</h2>
+<main class="l-main l-main-fr p-main-fr">
+  <section class="l-section-mv p-mainVisual-fr p-mainVisual">
+    <div>
+      <h2 class="c-text-mv c-text-mvTitle">ダミーサイト</h2>
+    </div>
+  </section>
+  <a class="p-card p-card-service p-card-service01" href="/index.php?cat=14">
+    <h2 class="c-text-cardTitle-fr c-deco-underLine">TakeOut</h2>
+    <div class="p-card-service__text">
+      <div class="p-card-service__inr">
+        <h3 class="c-text-card__inrTitle">Take Out</h3>
+        <p class="c-text-card__inrP">当店のテイクアウトで利用できる商品を掲載しています</p>
       </div>
-    </section>
-    <section class="p-card p-card-service p-card-service01">
-      <h2 class="c-text-cardTitle-fr c-deco-underLine">TakeOut</h2>
-      <div class="p-card-service__text">
-        <div class="p-card-service__inr">
-          <h3 class="c-text-card__inrTitle">Take OUT</h3>
-          <p class="c-text-card__inrP">当店のテイクアウトで利用できる商品を掲載しています</p>
-        </div>
-        <div class="p-card-service__inr">
-          <h3 class="c-text-card__inrTitle">Take OUT</h3>
-          <p class="c-text-card__inrP">当店のテイクアウトで利用できる商品を掲載しています</p>
-        </div>
+      <div class="p-card-service__inr">
+        <h3 class="c-text-card__inrTitle">Eat In</h3>
+        <p class="c-text-card__inrP">当店のテイクアウトで利用できる商品を掲載しています</p>
       </div>
-    </section>
-    <section class="p-card p-card-service p-card-service02">
-      <h2 class="c-text-cardTitle-fr c-deco-underLine">TakeOut</h2>
-      <div class="p-card-service__text">
-        <div class="p-card-service__inr">
-          <h3 class="c-text-card__inrTitle">Take OUT</h3>
-          <p class="c-text-card__inrP">当店のテイクアウトで利用できる商品を掲載しています</p>
-        </div>
-        <div class="p-card-service__inr">
-          <h3 class="c-text-card__inrTitle">Take OUT</h3>
-          <p class="c-text-card__inrP">当店のテイクアウトで利用できる商品を掲載しています</p>
-        </div>
+    </div>
+  </a>
+  <a class="p-card p-card-service p-card-service02" href="/index.php?cat=15">
+    <h2 class="c-text-cardTitle-fr c-deco-underLine">TakeOut</h2>
+    <div class="p-card-service__text">
+      <div class="p-card-service__inr">
+        <h3 class="c-text-card__inrTitle">Take OUT</h3>
+        <p class="c-text-card__inrP">当店のテイクアウトで利用できる商品を掲載しています</p>
       </div>
-    </section>
-    <section class="p-map">
-      <div class="p-map-overlay">
-        <h2 class="c-text-map c-text-mapTitle  c-deco-underLine">見出しが入ります</h2>
-        <p class="c-text-map c-text-mapp">
-          テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
-        </p>
+      <div class="p-card-service__inr">
+        <h3 class="c-text-card__inrTitle">Take OUT</h3>
+        <p class="c-text-card__inrP">当店のテイクアウトで利用できる商品を掲載しています</p>
       </div>
-    </section>
-  </main><!-- l-main -->
-  <?php get_footer(); ?>
+    </div>
+  </a>
+  <section class="p-map">
+    <div class="p-map-overlay">
+      <h2 class="c-text-map c-text-mapTitle  c-deco-underLine">見出しが入ります</h2>
+      <p class="c-text-map c-text-mapp">
+        テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
+      </p>
+    </div>
+  </section>
+</main><!-- l-main -->
+<?php get_footer(); ?>
